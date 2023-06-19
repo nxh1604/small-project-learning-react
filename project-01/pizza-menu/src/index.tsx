5283363da29fa1363eb4ec8dfa486dcfe69d58ab
GoldenAzusa <101515269+nxh1604@users.noreply.github.com>
2023-06-19 21:17:02 +0700

    working on my frist small project

---
+++ project-01/pizza-menu/src/index.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+const App = () => {
+  return <h1>Hello react!!!!!</h1>;
+};
+
+// React v18
+const root = ReactDOM.createRoot(
+  document.getElementById("root") as HTMLElement
+);
+
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
